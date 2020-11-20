70a6b052775e6469098f93e131a858bd233bbcb6
discowitch <disco.witchcraft@gmail.com>
2020-11-19 19:28:14 -0500

    Patch bag of holding spawn code compatibility. Add bag cycling code. Add bag usage tooltip.

--- files/items/bag_holding/bag.lua
+++ files/items/bag_holding/bag.lua
@@ -3,15 +3,11 @@ dofile_once("mods/azoth/files/lib/disco_util.lua")
 local self = Entity(GetUpdatedEntityID())
 
 local children = self:children()
-if children then
-    local contents = children:search(function(ent)
-        return ent.ItemComponent ~= nil
-    end)
-    if contents then
-        self.ItemComponent.uses_remaining = contents:len()
-    else
-        self.ItemComponent.uses_remaining = 0
-    end
+local contents = children and children:search(function(ent)
+    return ent.ItemComponent ~= nil
+end)
+if contents then
+    self.ItemComponent.uses_remaining = contents:len()
 else
     self.ItemComponent.uses_remaining = 0
 end
--- files/items/bag_holding/spawn_bag.lua
+++ files/items/bag_holding/spawn_bag.lua
@@ -1,7 +1,5 @@
+local _spawn_corpse = spawn_corpse
 function spawn_corpse(x, y)
-    EntityLoad("data/entities/props/physics_skull_01.xml", x, y - 4)
-    EntityLoad("data/entities/props/physics_bone_01.xml", x + 8, y - 4)
-    EntityLoad("data/entities/props/physics_bone_06.xml", x - 12, y - 4)
-    EntityLoad("data/entities/items/books/book_corpse.xml", x, y)
+    _spawn_corpse(x, y)
     EntityLoad("mods/azoth/files/items/bag_holding/bag.xml", x, y - 10)
 end
