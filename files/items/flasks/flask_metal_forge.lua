dee5de372437ddb41048c0235bc9eb39805fe720
discowitch <disco.witchcraft@gmail.com>
2020-11-26 12:50:37 -0500

    Add Metal flask -> vacuum flask forging

---
+++ files/items/flasks/flask_metal_forge.lua
@@ -0,0 +1,52 @@
+dofile_once("mods/azoth/files/lib/disco_util.lua")
+
+function MoveMaterials(from, to)
+    local mats = from.MaterialInventoryComponent.count_per_material_type
+    for k, v in ipairs(mats) do
+        local matname = CellFactory_GetName(k - 1)
+        AddMaterialInventoryMaterial(to:id(), matname, v)
+    end
+end
+
+function HasMaterial(item, mat_name, amount)
+    local mats = item.MaterialInventoryComponent.count_per_material_type
+    if type(mat_name) == "string" then
+        return mats[CellFactory_GetType(mat_name) + 1] > amount
+    else
+        local count = 0
+        for k, v in ipairs(mat_name) do count = count + mats[CellFactory_GetType(v) + 1] end
+        return count > amount
+    end
+end
+
+function CanConvert(fdata)
+    local item = fdata:parent()
+    return HasMaterial(item, {
+        "magic_liquid_teleportation",
+        "magic_liquid_unstable_teleportation",
+        "magic_liquid_elsewhere"
+    }, 500) or HasMaterial(item, {"magic_liquid_chaos"}, 500)
+end
+
+function Convert(fdata)
+    local item = fdata:parent()
+    local x, y = item:transform()
+    -- If the flask is full of teleportatium, make a vacuum flask
+    if HasMaterial(item, {
+        "magic_liquid_teleportation",
+        "magic_liquid_unstable_teleportation",
+        "magic_liquid_elsewhere"
+    }, 500) then
+        EntityLoad("mods/azoth/files/items/flasks/flask_vacuum.xml", x, y)
+    elseif HasMaterial(item, {"magic_liquid_chaos"}, 500) then
+        -- Create a bag of holding
+        EntityLoad("mods/azoth/files/items/bag_holding/bag.xml", x, y)
+    else
+        -- Otherwise, just return
+        return
+    end
+    item:kill()
+    converted = true
+end
+
+return {can_convert = CanConvert, convert = Convert}
