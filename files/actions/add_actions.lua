169cd6a2a5e675988102f5bd4dc06a21c20e0294
discowitch <disco.witchcraft@gmail.com>
2020-12-05 12:41:58 -0500

    Add debug build check for spawning test spell.

--- files/actions/add_actions.lua
+++ files/actions/add_actions.lua
@@ -9,4 +9,7 @@ dofile_once("mods/azoth/files/actions/larpa_field/init.lua")
 dofile_once("mods/azoth/files/actions/necromancy/init.lua")
 dofile_once("mods/azoth/files/actions/polymorph/init.lua")
 
-dofile_once("mods/azoth/files/actions/test/init.lua")
+-- Only add the test spell when debugging
+if DebugGetIsDevBuild and DebugGetIsDevBuild() then
+    dofile_once("mods/azoth/files/actions/test/init.lua")
+end
